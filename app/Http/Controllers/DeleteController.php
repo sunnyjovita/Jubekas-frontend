9119492ca84e3fc99ff6e1d4b9b5fc122df9fffa
Sunny Jovita <sunnyjovitaaa@gmail.com>
2021-06-23 15:12:43 +0700

    Update DeleteController.php

--- app/Http/Controllers/DeleteController.php
+++ app/Http/Controllers/DeleteController.php
@@ -12,16 +12,16 @@ use Redirect;
 class DeleteController extends Controller
 {
     //
-    public function deleteCars($id, Request $req){
+    public function deleteCars($id){
 
     	$cars = Http::delete(env('API_URL')."api/delete/car/$id");
 
-        // $id = Session::get('id');
-         $req->session()->flash('success', 'successfully deleted ur product');
+         $id = Session::get('id');
+//         $req->session()->flash('success', 'successfully deleted ur product');
 
 //         return view('profile.user-profile');
-//        return redirect()->to('user-profile/products/'.$id);
-         return view('profile.user-profile',['cars'=>$cars]);
+        return redirect()->to('user-profile/products/'.$id);
+//         return view('profile.user-profile',['cars'=>$cars]);
 
 
     }
